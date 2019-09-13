ba0e0eaa5ce6b06991ad543ad9443c973bbb99cd
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-13 10:55:25 -0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarEvents
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+        // This delegate works in conjunction with the Car's events.
+        public delegate void CarEngineHandler(string msg);
+
+        // This car can send these events.
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        public void Accelerate(int delta)
+        {
+            // If the car is dead, fire Exploded event.
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                    Exploded("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                // Almost dead?
+                if (10 == MaxSpeed - CurrentSpeed && AboutToBlow != null)
+                {
+                    AboutToBlow("Careful buddy! Gonna blow!");
+                }
+                // Still OK!
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
